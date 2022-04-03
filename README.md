e32d01b014ef8bf0bfcfbcce4ec0714b788df28c
Michael Meoak <mmeoak240@gmail.com>
2022-04-02 17:54:56 -0700

    created nav bar, colored background

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Please please please work
+Idea:
+Create an application that will randomly generate a quote from NBC's The Office.
+There will be a button for each character in the show for the user to submit as their guess as to who the quote belongs to.
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,19 @@
-alert("hello world");
+// const quizContainer = document.getElementById("quiz");
+// const resultsContainer = document.getElementById("results");
+// const submitButton = document.getElementById("submit");
+
+// function buildQuiz() {}
+
+// function showResults() {}
+
+// // display quiz right away
+// buildQuiz();
+
+// // on submit, show results
+// submitButton.addEventListener("click", showResults);
+
+// function quoteGetter() {
+// 	fetch("https://officeapi.dev/api/quotes/random")
+// 		.then((resp) => resp.json())
+// 		.then((quoteObj) => renderQuote(quoteObj));
+// }
