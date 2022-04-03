8a1195fcc28f7e7acda0a1c416355e2c419460ad
Michael Meoak <mmeoak240@gmail.com>
2022-04-02 17:57:59 -0700

    created working "Home" button in nav bar

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,12 +1,40 @@
 /**Global Variables */
 
 /**Node Getters*/
-
+const mainDiv = () => document.getElementById("main");
+const homeLink = () => document.getElementById("home-link");
 /**Event Listeners*/
+const attachHomeLink = () => {
+	homeLink().addEventListener("click", loadHome);
+};
+
+/**Event Handlers*/
+const loadHome = () => {
+	resetMainDiv();
+	const h1 = document.createElement("h1");
+	const p = document.createElement("p");
+
+	h1.innerText = "That's What Who Said!";
+	p.innerText = "How well do you know your Office family?";
+
+	h1.className = "center-align";
+	p.className = "center-align";
+
+	mainDiv().appendChild(h1);
+	mainDiv().appendChild(p);
+};
 
 /**MISC*/
+const resetMainDiv = () => {
+	mainDiv().innerHTML = "";
+};
 
 /**StartUp*/
+
+document.addEventListener("DOMContentLoaded", function () {
+	loadHome();
+	attachHomeLink();
+});
 
 // const quizContainer = document.getElementById("quiz");
 // const resultsContainer = document.getElementById("results");
