f6294f19019f6c277a3a82cbf9b144e78519a103
Michael Meoak <mmeoak240@gmail.com>
2022-04-05 10:44:44 -0700

    deleted old unused code at bottom

--- javascripts/index.js
+++ javascripts/index.js
@@ -117,6 +117,7 @@ const quoteGetter = () => {
 				console.log(quote);
 				const p = document.createElement("p");
 				p.className = "container";
+				p.className = "quotePar";
 				p.textContent = quote;
 				mainDiv().appendChild(p);
 			});
@@ -136,23 +137,3 @@ document.addEventListener("DOMContentLoaded", function () {
 	attachBeginLink();
 	attachCreditsLink();
 });
-
-// const quizContainer = document.getElementById("quiz");
-// const resultsContainer = document.getElementById("results");
-// const submitButton = document.getElementById("submit");
-
-// function buildQuiz() {}
-
-// function showResults() {}
-
-// // display quiz right away
-// buildQuiz();
-
-// // on submit, show results
-// submitButton.addEventListener("click", showResults);
-
-// function quoteGetter() {
-// 	fetch("https://officeapi.dev/api/quotes/random")
-// 		.then((resp) => resp.json())
-// 		.then((quoteObj) => renderQuote(quoteObj));
-// }
